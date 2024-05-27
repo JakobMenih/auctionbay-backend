5e72298d184b718107e3b9a42b9c08c80c401fed
Jakob Menih <jakob.menih@gmail.com>
2024-05-27 21:27:44 +0200

    Connecting the backend to the database, adding directories and files.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ModuleModule } from './module/module.module';
+import { AuthModule } from './auth/auth.module';
+import { AuctionModule } from './auction/auction.module';
+import { UserModule } from './user/user.module';
+import { UsernpmModule } from './install/usernpm/usernpm.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5433,
+      username: 'postgres',
+      password: 'admin',
+      database: 'auctionbay',
+      entities: ['dist/**/*.entity.js'],
+      synchronize: true,
+    }),
+    ModuleModule,
+    AuthModule,
+    AuctionModule,
+    UserModule,
+    UsernpmModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
